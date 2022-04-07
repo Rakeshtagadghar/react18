ecdf3fe85b84d0b1132100c78202bba3362b7876
rakesh T <rakesh.tagadghar@dltapps.co.uk>
2022-04-07 14:42:31 +0530

    google capchta

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useCallback } from "react";
+import ReactDOM from "react-dom";
+import {
+  GoogleReCaptchaProvider,
+  useGoogleReCaptcha,
+} from "react-google-recaptcha-v3";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const YourReCaptchaComponent = () => {
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
+  // Create an event handler so you can call the verification on button click event or form submit
+  const handleReCaptchaVerify = useCallback(async () => {
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not yet available");
+      return;
+    }
+
+    const token = await executeRecaptcha("yourAction");
+    // Do whatever you want with the token
+    console.log(token);
+  }, [executeRecaptcha]);
+
+  // You can use useEffect to trigger the verification as soon as the component being loaded
+  useEffect(() => {
+    handleReCaptchaVerify();
+  }, [handleReCaptchaVerify]);
+
+  return <button onClick={handleReCaptchaVerify}>Verify recaptcha</button>;
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GoogleReCaptchaProvider reCaptchaKey="6LcqFVMfAAAAABgfXKBR31Z0BsLkpmvnOUbwSz7U">
+      <YourReCaptchaComponent />
+      <App />
+    </GoogleReCaptchaProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
